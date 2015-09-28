ab45cdd41f54f4853e439cf29e0bce22ccef9af5
= <=>
2015-09-28 10:08:46 -0400

    Server makes the tables itself, instead of inserting the SQL.

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -130,6 +130,31 @@ function GM:Initialize()
 	return self.BaseClass:Initialize()
 end
 
+function MakeTables()
+	timer.Simple(5, function()
+		if self.DB then
+		self.DB:query([[
+			CREATE TABLE IF NOT EXISTS `players`
+			`_RPName` longtext NOT NULL,
+			`_Key` int(11) NOT NULL,
+			`_Name` longtext NOT NULL,
+			`_Clan` longtext NOT NULL,
+			`_Description` longtext NOT NULL,
+			`_SteamID` longtext NOT NULL,
+			`_UniqueID` longtext NOT NULL,
+			`_Access` longtext NOT NULL,
+			`_Donator` longtext NOT NULL,
+			`_Arrested` longtext NOT NULL,
+			`_Inventory` longtext NOT NULL,
+			`_Blacklist` longtext NOT NULL,
+			`_Misc` longtext NOT NULL,
+			PRIMARY KEY (`_Key`)
+		]])
+		end
+	end)
+end
+hook.Add("PostGamemodeLoaded", "MakeSQLTables", MakeTables)
+
 
 function GM:Query(query, callback)
 	if self.DB and (query) then
