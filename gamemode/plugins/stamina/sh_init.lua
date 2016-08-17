f302705590f4f3bfa5678ba83bc4864f56d456eb
Pawsative <seftonswow@gmail.com>
2016-08-17 04:48:57 -0400

    yea

--- gamemode/plugins/stamina/sh_init.lua
+++ gamemode/plugins/stamina/sh_init.lua
@@ -4,12 +4,3 @@
 --]]
 
 PLUGIN.Name = "Stamina";
-
-
-function AntiJump(ply, bind, pressed)
-	local stamina = ply._Stamina or 0
-	if bind == "+jump" and pressed and stamina < 10 then
-		return true
-	end
-end
-hook.Add("PlayerBindPress", "AntiJump", AntiJump)
