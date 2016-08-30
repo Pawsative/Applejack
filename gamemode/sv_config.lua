71aa7706918aa4a57ce562d57224265d545b2ea6
AdamDeJames <adamdejames@gmail.com>
2016-08-30 18:49:46 +0900

    Forgot to remove my db password lel
    password changed dont try anything lmao

--- gamemode/sv_config.lua
+++ gamemode/sv_config.lua
@@ -6,6 +6,6 @@
 --Remove the commented out lines
 GM.Config["MySQL Host"] = "208.146.35.14"; -- The host that your MySQL database is located.
 GM.Config["MySQL Username"] = "deltasrp"; -- The username that you log into MySQL with.
-GM.Config["MySQL Password"] = "gvxe5wZxJv"; -- The password that you log into MySQL with.
+GM.Config["MySQL Password"] = "3123213123123"; -- The password that you log into MySQL with.
 GM.Config["MySQL Database"] = "deltasrp_applejack"; -- The name of the database that we'll be using.
 GM.Config["MySQL Table"] = "players"; -- The name of the table where the player data is stored.
